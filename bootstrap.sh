ec8031b2092f61541b1f65fa5da1248f00e857c8
Jason Sherman <jsnshrmn@users.noreply.github.com>
2015-04-30 21:07:11 -0500

    fixed lynx install

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,9 +18,8 @@ sudo apt-get -y install openssh-server
 sudo apt-get -y install build-essential automake libtool
 
 # Utilities
-sudo apt-get -y install git wget curl tree psmisc emacs24-nox vim ack-grep tmux screen
+sudo apt-get -y install git wget curl tree psmisc emacs24-nox vim ack-grep tmux screen lynx
 sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
-sudo apt-get install lynx
 
 # vmfriendly ntpd replacement
 sudo apt-get -y install chrony
